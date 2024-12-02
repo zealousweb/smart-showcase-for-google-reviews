2c39015f62f2bcbf351df41b73aa25c5e98dd84b
Pooja <pooja.faldu@zealousweb.com>
2024-12-02 16:20:43 +0530

    Fix css issue for slider 5
    Fix css issue for slider 5

--- inc/admin/class.zwsgr.admin.action.php
+++ inc/admin/class.zwsgr.admin.action.php
@@ -1093,7 +1093,7 @@ if ( !class_exists( 'ZWSGR_Admin_Action' ) ){
 
 						$zwsgr_slider_item5 = '
 							<div class="zwsgr-slide-item">
-								<div class="zwsgr-list-inner">
+								<div class="">
 									<div class="zwsgr-profile">
 										<img src="' . $plugin_dir_path . 'assets/images/testi-pic.png">	
 										<div class="zwsgr-google-icon">
